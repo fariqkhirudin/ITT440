3f669a07bce84261af1ef8fcab8628c48c0c7fae
fariq khirudin <fariqkhirudin@gmail.com>
2020-05-17 02:43:32 +0800

    update

--- portScanner4.py
+++ portScanner4.py
@@ -3,19 +3,22 @@ from socket import gethostbyname
 from datetime import *
 
 
-def scanner(host, port):
+
+
+
+def scanner(port):
+
     s = socket.socket()
     s.settimeout(0.1)
 
     try:
         s.connect((host, port))
+        return True
     except:
         return False
-    else:
-        return True
 
 host = input("Enter the host:")
-ip = socket.gethostbyname(host)
+ip = socket.gethostbyname(host) #convert url to ipv4
 
 rangeMin = int(input("Min range to scan: "))
 rangeMax = int(input("Max range to scan: "))
@@ -23,12 +26,12 @@ rangeMax = int(input("Max range to scan: "))
 t1 = datetime.now()
 
 for port in range(rangeMin, rangeMax + 1):
-    if scanner(ip, port):
+    if scanner(port):
         print("Port ", port, ": Open")
-
-t2 = datetime.now()
-
-totalTime = t2-t1
 #    else:
 #        print("Port ", port, ": Closed")
+
+t2 = datetime.now()
+totalTime = t2-t1
+
 print("Scan completed in ", totalTime )
